83c23cf696e3e4b111d152d93435b99c8e41eb1a
Samitha Bandara <122595628+samitha278@users.noreply.github.com>
2025-02-15 10:52:41 +0530

    Update q1.cpp

--- week3/take home lab/q1.cpp
+++ week3/take home lab/q1.cpp
@@ -2,18 +2,6 @@
 
 using namespace std;
 
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
-
-/*
- * Complete the 'superDigit' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts following parameters:
- *  1. STRING n
- *  2. INTEGER k
- */
 
 int superDigit(string n, int k) {
     
@@ -22,84 +10,28 @@ int superDigit(string n, int k) {
         return stoi(n);
     }
     
-    //create long string
-    string temp = n;
-    if(k>1){
-        for(int i = 1 ; i<k ; i++){
-            temp+=n ;
-        }
+    //calculate value
+    long long sum = 0;
+    for(char ch : n){
+        sum += (ch - '0');
     }
     
-    //calculate value
-    int value = 0;
-    for(auto &ch:temp){
-        int t = ch - '0';
-        value += t;
-    }
+    sum *= k;
     
     //recursion case
-    return superDigit(to_string(value),1);
+    return superDigit(to_string(sum),1);
 }
 
 
 
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string first_multiple_input_temp;
-    getline(cin, first_multiple_input_temp);
-
-    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
-
-    string n = first_multiple_input[0];
-
-    int k = stoi(first_multiple_input[1]);
-
-    int result = superDigit(n, k);
-
-    fout << result << "\n";
-
-    fout.close();
-
-    return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
+int main(){
+    
+    
+    int n,k;
+    cin>>n>>k;
+    
+    cout<<superDigit(to_string(n),k);
+    
+    
+    
 }
