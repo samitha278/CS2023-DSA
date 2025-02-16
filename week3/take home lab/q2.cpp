a575e90c37fdb6b359b67e4691549765d98c6a4c
Samitha Bandara <122595628+samitha278@users.noreply.github.com>
2025-02-16 12:39:09 +0530

    Update q2.cpp

--- week3/take home lab/q2.cpp
+++ week3/take home lab/q2.cpp
@@ -7,21 +7,47 @@ int powerSum(double x,double n){
     
     double k = pow(x,1.0/n);
     
-    return k;
+    int paths = 0;
     
+    
+    int func(int bal,int pre ){
+        
+        if(bal == 0){
+            return 0;
+        }
+        if(pre==1){
+            return 1;
+        }
+        
+        return func()
+        
+           
+    }
+    
+    
+    
+    for(int i = k ; i > 0 ; i--){
+        
+        int bal = x - pow(i,n);
+        
+        int val = func(bal,i);
+        
+        if(val==0){
+            paths++;
+        }
+        
+    }
+   
+   
     
 }
 
-
-
-
 int main()
 {
-    
-    int x,n;
+    double x,n;
     cin>>x>>n;
     
     cout << powerSum(x,n);
-    
+
     return 0;
 }
