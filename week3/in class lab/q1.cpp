a3f2fc647001867084f1e6882b5983dff620c993
Samitha Sahan <122595628+samitha278@users.noreply.github.com>
2025-02-21 18:59:15 +0530

    Update q1.cpp

--- week3/in class lab/q1.cpp
+++ week3/in class lab/q1.cpp
@@ -3,31 +3,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> mergeSort(vector<int> a , int n){
 
-    if(n==1){
-        return a;
-    }
-    
-    int temp = 0;
-    if(n%2==0){
-        temp = n/2;
-    }else{
-        temp = (n-1)/2;
-    }
-    
-    
-    vector<int> l(a.begin(),a.begin()+temp);
-    vector<int> r(a.begin()+temp,a.begin()+n);
-    mergeSort(l,l.size());
-    mergeSort(r,r.size());
-    
-    
-    merge(l,r,a);
-    
-    return a;
-        
-}
 
 void merge(vector<int> &l , vector<int> &r , vector<int> &a){
     
@@ -38,7 +14,7 @@ void merge(vector<int> &l , vector<int> &r , vector<int> &a){
     int len_r = r.size();
     int len_a = a.size();
     
-    while(i<len_l && j<<len_r){
+    while(i<len_l && j<len_r){
         if(l[i]<=r[j]){
             a[k] = l[i];
             i++;
@@ -59,8 +35,42 @@ void merge(vector<int> &l , vector<int> &r , vector<int> &a){
         j++;
         k++;
     }
-        
+    
+    
 }
+
+
+vector<int> mergeSort(vector<int> a , int n){
+
+
+    if(n==1){
+        return a;
+    }
+    
+    int temp = 0;
+    if(n%2==0){
+        temp = n/2;
+    }else{
+        temp = (n-1)/2;
+    }
+    
+    
+    vector<int> l(a.begin(),a.begin()+temp);
+    vector<int> r(a.begin()+temp,a.begin()+n);
+    vector<int> l_new = mergeSort(l,l.size());
+    vector<int> r_new = mergeSort(r,r.size());
+    
+    
+    merge(l_new,r_new,a);
+    
+    return a;
+    
+    
+}
+
+
+
+
 
 int main(){
     
@@ -72,10 +82,11 @@ int main(){
     for(int &ele : a){
         cin >> ele;
     }
-      
-    mergeSort(a,n);
     
-    for(const int &ele : a ){
+    
+    vector<int> a_new = mergeSort(a,n);
+    
+    for(const int &ele : a_new ){
         cout << ele << " ";
     }
     
